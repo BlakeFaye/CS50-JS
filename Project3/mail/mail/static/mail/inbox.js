d49b196f55bf03501b85439bb472414435c9df4f
BlakeFaye <elise.estienne@laposte.net>
2025-12-16 16:48:59 +0200

    feat: display infos about a mail

--- Project3/mail/mail/static/mail/inbox.js
+++ Project3/mail/mail/static/mail/inbox.js
@@ -12,8 +12,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function compose_email() {
 
-  console.log("BBB")
-
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
@@ -25,9 +23,7 @@ function compose_email() {
 }
 
 function load_mailbox(mailbox) {
-
-  console.log("AAA")
-  
+ 
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
@@ -35,11 +31,53 @@ function load_mailbox(mailbox) {
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
+  loop1: for (let i = 1; i<10; i++) //i<100 T-T
+    {
+      
+      fetch(`/emails/${i}`)
+      .then(response => response.json())
+      .then(email => {
+      console.log(email);
+      console.log(i)
+      //console.log(email.hasOwnProperty("error"));
+      if (email.hasOwnProperty("error")){
+        console.log("coucou");
+      }
+    })
+      }
+
+      
   fetch('/emails/1')
   .then(response => response.json())
   .then(email => {
-    // Print email
     console.log(email);
+
+    //if error key found in returned JSON print it
+    if (email.hasOwnProperty("error")){ 
+      console.log(email.error);
+      document.querySelector('#emails-view').innerHTML = document.querySelector('#emails-view').innerHTML + "Error: " + email.error;
+    }
+    //otherwise, print mails
+    else { 
+      const mel = email;
+      const read = "No";
+      const archived = "No";
+      if (mel.read) {
+        read = "Yes"
+      }
+      if (mel.archived) {
+        read = "Yes"
+      }
+
+      document.querySelector('#emails-view').innerHTML = document.querySelector('#emails-view').innerHTML  +  
+      `<p> Subject : ${mel.subject}</p>
+      <p> Sender : ${mel.sender}</p>
+      <p> Timestamp : ${mel.timestamp}</p>
+      <p> Body : ${mel.body}</p>
+      <p> Read? : ${read} | Archived? : ${archived}</p>
+      ____________
+      `
+    }
   }
   )
 }
