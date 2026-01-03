9e75ef4c49e9477aa6a76be12a7d089d55c0a69f
BlakeFaye <elise.estienne@laposte.net>
2026-01-03 11:32:38 +0200

    feat: archive function, display recipients

--- Project3/mail/mail/static/mail/inbox.js
+++ Project3/mail/mail/static/mail/inbox.js
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
   load_mailbox('inbox');
 });
 
+
 function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
@@ -21,19 +22,17 @@ function compose_email() {
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
 
+  // Compose mail
+  // Adblockers must be unabled for the composition form to work
   document.querySelector('#submit-compose-form').onclick = () => {
     const new_mail_recipients = document.querySelector('#compose-recipients').value;
     const new_mail_subject = document.querySelector('#compose-subject').value;
     const new_mail_body = document.querySelector('#compose-body').value;
 
-    console.log (new_mail_subject);
-    console.log (new_mail_recipients);
-    console.log (new_mail_body);
-
+    //Get a list of all recipients
     var recs = new_mail_recipients.split(", ");
     console.log(recs);
-
-    // pour que ça fonctionne, il faut désactiver ublock et abp
+    
     fetch('/emails',{
       method: "POST",
       body: JSON.stringify({
@@ -47,9 +46,11 @@ function compose_email() {
     };
 }
 
+
 function view_mail(mail_id){
   console.log (mail_id)
 
+  //Display the mail viewer and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#single-mail-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
@@ -57,28 +58,61 @@ function view_mail(mail_id){
   fetch(`/emails/${mail_id}`)
     .then(response => response.json())
     .then(email => {
-      console.log(email)
-        document.querySelector('#single-mail-view').innerHTML = 
+
+      //Display mail infos
+      document.querySelector('#single-mail-view').innerHTML = 
       `
       <p> Single email view </p>
       <p> Subject : ${email.subject}</p>
       <p> Sender : ${email.sender}</p>
+      <p> Recipients : ${email.recipients}</p>
       <p> Timestamp : ${email.timestamp}</p>
       <p> Body : ${email.body}</p>
       <p> Read? : ${email.read} | Archived? : ${email.archived}</p>
       <p> BBBBBBB </p>
       `;
 
-    if (!email.read){
-      fetch(`/emails/${mail_id}`,{
-      method: "PUT",
-      body: JSON.stringify({
-        read: true
-      })
+      //Switch to read
+      if (!email.read){
+        fetch(`/emails/${mail_id}`,{
+        method: "PUT",
+        body: JSON.stringify({
+          read: true
+        })
+        }
+      )
       }
-    )
-    }
 
+      //Archive logic
+      const archive_button = document.createElement('button');
+      if (email.archived) {
+        archive_button.innerHTML = "Unarchive";
+        archive_button.addEventListener('click', function() {
+            fetch(`/emails/${mail_id}`,{
+              method: "PUT",
+              body: JSON.stringify({
+                archived: false
+              })
+            })
+            .then(() => {load_mailbox('inbox')})
+        })
+      } 
+
+      else {
+        archive_button.innerHTML = "Archive";
+        archive_button.addEventListener('click', function() {
+            fetch(`/emails/${mail_id}`,{
+              method: "PUT",
+              body: JSON.stringify({
+                archived: true
+              })
+            })
+            .then(() => {load_mailbox('archive')})
+        })
+      }
+      
+      
+    document.querySelector('#single-mail-view').append(archive_button);
     document.querySelector('#emails-view').style.display = 'none';
     document.querySelector('#single-mail-view').style.display = 'block';
     document.querySelector('#compose-view').style.display = 'none';
@@ -99,7 +133,6 @@ function load_mailbox(mailbox) {
 
   console.log("mailbox: " + mailbox)
 
-
   fetch(`/emails/${mailbox}`)
     .then(response => response.json())
     .then(emails =>
@@ -115,6 +148,7 @@ function load_mailbox(mailbox) {
         single_mail.innerHTML = 
       `<p> Subject : ${mel.subject}</p>
       <p> Sender : ${mel.sender}</p>
+      <p> Recipients : ${mel.recipients}</p>
       <p> Timestamp : ${mel.timestamp}</p>
       <p> Body : ${mel.body}</p>
       <p> Read? : ${mel.read} | Archived? : ${mel.archived}</p>
