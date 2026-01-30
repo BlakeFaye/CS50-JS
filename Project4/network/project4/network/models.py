81422244d9bb23d3ab40a0a53181f75aeee39d98
BlakeFaye <elise.estienne@laposte.net>
2026-01-30 11:50:06 +0100

    feat: Create small post form

--- Project4/network/project4/network/models.py
+++ Project4/network/project4/network/models.py
@@ -4,3 +4,10 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    user = user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="emails")
+    timestamp = models.DateTimeField(auto_now_add=True)
+    content = models.TextField(blank=True)
+
+    #TODO : Serialize pour timestamp ?
