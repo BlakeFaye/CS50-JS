f80d7971104c93ebacc6059ec8106816adf74280
Blake_Laptop <elise.estienne@laposte.net>
2026-03-14 11:04:01 +0100

    feat: post viewing beginning

--- Project4/network/project4/network/static/network/network_all_posts.js
+++ Project4/network/project4/network/static/network/network_all_posts.js
@@ -2,9 +2,33 @@ document.addEventListener('DOMContentLoaded', function() {
     load_posts();
 });
 
-// function load_posts(){
-//     document.querySelector('#all-posts-view').style.display = 'block';
-//     fetch(`/all-posts/`)
-//     .then(response => response.json())
-//     .then(email => {
-// }
+function load_posts(){
+    document.querySelector('#all-posts-view').style.display = 'block';
+
+  fetch(`/all_posts_data`)
+    .then(response => response.json())
+    .then(posts =>
+        {
+            for (let i = 0; i < posts.length; i++)
+            {
+                console.log(i)
+                console.log(posts[i])
+            }
+            
+        }
+    )
+}
+
+function view_post(post_id){
+    fetch(`/post/${post_id}`)
+    .then(response => response.json())
+    .then(post => {
+        document.querySelector('#all-posts-view').innerHTML = 
+      `
+      <p> Single post view </p>
+      <p> Content : ${post.content}</p>
+      <p> Timestamp : ${post.timestamp}</p>
+      <p> BBBBBBB </p>
+      `;
+    })
+}
